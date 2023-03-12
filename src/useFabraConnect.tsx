cc5795ec153765d50b45b62146fc447f88140950
teddarific <teddyni19@gmail.com>
2023-03-12 13:18:26 -0700

    Update with main app updates

--- src/useFabraConnect.tsx
+++ src/useFabraConnect.tsx
@@ -1,31 +1,56 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import useScript from 'react-script-hook';
 
 declare global {
   interface Window { fabra: FabraConnect; }
 }
 
+export interface CustomTheme {
+  colors?: {
+    primary: {
+      base: string; // Primary theme color
+      hover: string; // Color when hovering over primary buttons
+      text: string; // Text color on top of the primary color
+    }
+  }
+}
+
 export interface FabraConnect {
-  open: (linkToken: string) => void;
+  initialize: ({ linkToken, customTheme} : { linkToken: string, customTheme?: CustomTheme }) => Promise<void>;
+  open: () => void;
   close: () => void;
 }
 
+
 export type UseFabraConnectResponse = {
-  open: (linkToken: string) => void;
+  open: (linkToken: string, customTheme?: CustomTheme) => void;
   close: () => void;
 };
 
-export const useFabraConnect = (): UseFabraConnectResponse => {
+export const useFabraConnect = ({ linkToken, customTheme } :{ linkToken: string, customTheme?: CustomTheme}): UseFabraConnectResponse => {
+  const [isFabraReady, setIsFabraReady] = useState<boolean>(false)
+
+  const initFabra = useCallback(async () => {
+    await window.fabra.initialize({
+      linkToken,
+      customTheme,
+    });
+    
+    setIsFabraReady(true);
+  }, [linkToken, customTheme])
+
+
   useScript({
     src: 'https://connect.fabra.io/initialize.js',
     checkForExisting: true,
+    onload: () => {
+      initFabra()
+    }
   });
 
   const open = useCallback((linkToken: string) => {
     if (window.fabra) {
-      window.fabra.open(linkToken);
-    } else {
-
+      window.fabra.open();
     }
   }, []);
 
@@ -33,5 +58,9 @@ export const useFabraConnect = (): UseFabraConnectResponse => {
     window.fabra.close();
   };
 
-  return { open, close };
+  return { 
+    open, 
+    close, 
+    // isReady: isFabraReady, // Can expose this if needed for loading states. Plaid does this for example.
+  };
 };
